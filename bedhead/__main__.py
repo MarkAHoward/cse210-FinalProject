71640ef27674a0425246eac3200453b94550eb13
Conner Haar <77696186+Connerhaar@users.noreply.github.com>
2021-03-24 14:52:56 -0600

    stuff

--- bedhead/__main__.py
+++ bedhead/__main__.py
@@ -6,6 +6,7 @@ from game import constants
 def main():
 
     window = arcade.Window(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT, constants.SCREEN_TITLE)
+    
     game_view = Director()
     window.show_view(game_view)
     game_state = GameState()
