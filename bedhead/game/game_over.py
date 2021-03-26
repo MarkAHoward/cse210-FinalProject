dac5adfaa20f177341488167123688d55b1bf053
Conner Haar <77696186+Connerhaar@users.noreply.github.com>
2021-03-25 21:07:32 -0600

    screen fixes

--- bedhead/game/game_over.py
+++ bedhead/game/game_over.py
@@ -8,10 +8,10 @@ class GameOverView(arcade.View):
         """ This is run once when we switch to this view """
         super().__init__()
         self.texture = arcade.load_texture("cse210-FinalProject/bedhead/assets/images/game_over.png")
+        self.views = views
 
         # Reset the viewport, necessary if we have a scrolling game and we need
         # to reset the viewport back to the start so we can see what we draw.
-        arcade.set_viewport(0, constants.SCREEN_WIDTH - 1, 0, constants.SCREEN_HEIGHT - 1)
 
     def on_draw(self):
         """ Draw this view """
@@ -21,9 +21,7 @@ class GameOverView(arcade.View):
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
         """ If the user presses the mouse button, re-start the game. """
-        game_view = GameView()
-        game_view.setup(game_state)
-        self.window.show_view(game_view)
+        self.views['window'].show_view(self.views['start_screen'])
         # issues with getting the director and game_state to run from here, you cant 
         # import them without having an endless loop or have them as parameters because it doesnt work
         # so there is no way to retrieve there data
