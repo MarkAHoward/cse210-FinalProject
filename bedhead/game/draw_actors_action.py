90897215625c8b874ee36b697ed8f67f1e485ad9
Mark <77358956+MarkAHoward@users.noreply.github.com>
2021-04-02 14:53:25 -0600

    sped game up

--- bedhead/game/draw_actors_action.py
+++ bedhead/game/draw_actors_action.py
@@ -12,17 +12,24 @@ class DrawActorsAction(Action):
         self._output_service.start_screen()
 
         for group in cast['background']:
+            self._output_service.draw_actor(group[0])
+            self._output_service.draw_actor(group[1])
             for actor in group:
-                self._output_service.draw_actor(actor)
-
+                if actor.center_x < cast["player"][0].center_x + 600 and actor.center_x > cast["player"][0].center_x - 600:               
+                    self._output_service.draw_actor(actor)    
         for group in cast:
             if group != "score" and group != "items" and group != "player" and group != "background" and group != "decorations" and group != "invisible":
                 for actor in cast[group]:
-                    self._output_service.draw_actor(actor)
+                    if actor.center_x < cast["player"][0].center_x + 600 and actor.center_x > cast["player"][0].center_x - 600:
+                            self._output_service.draw_actor(actor)
                     
         self._output_service.draw_actor(cast["player"][0])
-        self._output_service.draw_actors(cast["decorations"])
+
+        for actor in cast['decorations']:
+            if actor.center_x < cast["player"][0].center_x + 600 and actor.center_x > cast["player"][0].center_x - 600:
+                self._output_service.draw_actor(actor)
         Score = cast[group][0]
+
         self._output_service.write_score(Score.get_score_text() , self._screen_scrolling.view_left, self._screen_scrolling.view_bottom )
 
         
